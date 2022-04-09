c8b2786d42b601a023809e6ba542ae17a15c45d9
Irtza <irtzatariq23@gmail.com>
2022-04-09 23:30:58 +0500

    ad backend

--- backend/models/ad.js
+++ backend/models/ad.js
@@ -4,15 +4,26 @@ const Schema = mongoose.Schema;
 //import {Blob} from 'buffer';
 let user = require('./user.js')
 
+
+
+const AnimalSchema = new Schema({
+    type: {type: Number, required:true},
+    weight: {type: Number, required:true},
+    sex: {type: String, required:true},
+    price: {type: Number, required:true}
+})
+
 const AdSchema = new Schema({
+    
     description: {type: String, required: true},
    // photo: {type: Blob},
     //video: {type: Blob},
     sold: {type: Boolean, required: true},
     seller_id: {type: Schema.Types.ObjectId, ref: user.Seller},
-    animal_id: {type: Number, required: true},
+    animal_id: {type: Schema.Types.ObjectId, required: true, ref: 'Animal'},
     ad_type: {type: Number, required: true}
 })
 
 const Ad = mongoose.model('Ad', AdSchema);
-module.exports = {Ad};
+const animal = mongoose.model('Animal',AnimalSchema);
+module.exports = {Ad,animal};
