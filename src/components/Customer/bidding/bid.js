e42ef3861910e8098fcee2faaf97a20539a0fff5
Haseeb <haseeb1399@gmail.com>
2022-04-13 04:39:47 +0500

    Revert "added bid css"
    This reverts commit 2f5fa760c66b20bceee4db00faed15a64b735a7b.
    
    getting back packages.json.lock

--- src/components/Customer/bidding/bid.js
+++ src/components/Customer/bidding/bid.js
@@ -6,49 +6,37 @@ import axios from "axios";
 const Bid = () => {
   return (
     <div class="App">
-      <div class="title-bid">Advertisement ID: insert id</div>
+      <div>Advertisement ID: insert id</div>
 
-      <div class="first-bid">
-        <div class="subtitle-bid">DETAILS</div>
-        <div class="break"></div>
-          <div class="bid-details">
-            <div class="bid-text">
-              <p>Sex: sex here</p>
-              <p>No. of Teeth: teeth here</p>
-              <p>Weight: weight here</p>
-              <p>Color: colors here</p>
-              <p>Breed: breed here</p>
-              <p>Age: age here</p>
-              <p>injuries: injury here</p>
-            </div>
-            <a class="maps" target={"_blank"} href="https://www.google.com/maps/place/Sohrab+Goth+Gaye+Mandi/@24.9912912,67.164752,15z/data=!4m2!3m1!1s0x0:0xe0b72951539871aa?sa=X&ved=2ahUKEwjViZ7Lvon3AhVBRBoKHTM5CWUQ_BJ6BAg1EAU">
-                  <img src={image}></img>
-            </a>
-          </div>
-        <div>
-            
+      <div className="first">
+        <div>Details</div>
+        <div className="third">
+            <div>Sex: sex here</div>
+            <div>No. of Teeth: teeth here</div>
+            <div>Weight: weight here</div>
+            <div>Color: colors here</div>
+            <div>Breed: breed here</div>
+            <div>Age: age here</div>
+            <div>injuries: injury here</div>
         </div>
-        <div>images from database should come here</div>
+        <div className="fourth">
+            <a target={"_blank"} href="https://www.google.com/maps/place/Sohrab+Goth+Gaye+Mandi/@24.9912912,67.164752,15z/data=!4m2!3m1!1s0x0:0xe0b72951539871aa?sa=X&ved=2ahUKEwjViZ7Lvon3AhVBRBoKHTM5CWUQ_BJ6BAg1EAU">
+                <img src={image}></img>
+            </a>
+        </div>
+        <div>images from database should come</div>
       </div>
 
-      <div class="second-bid">
-        <div class="subtitle-bid">BIDDING</div>
-        <div class="break"></div>
-        <div class="bid-options">
-          <div class="bid-text">
-            <p>Starting Price: price here</p>
-            <p>Start Time: time here</p>
-            <p>End Time: time here</p>
-            <p>Current Bid: bid here</p>
-            <p>Time Since Last Bid: time here</p>
-          </div>
-        </div>
-        <div class="break"></div>
-        <div class="bidding">
-            <p>Place Your Bid: <button class="button">Bid 1</button> <button class="button">Bid 2</button> <button class="button">Bid 3</button> </p> 
-            <p>OR</p>
-            <p><input class="input-bid" placeholder="Type Here"></input> <button class="button">Bid</button></p>
-        </div>
+      <div className="second">
+        <div>Bidding</div>
+        <div>Starting Price: price here</div>
+        <div>Start Time: time here</div>
+        <div>End Time: time here</div>
+        <div>Current Bid: bid here</div>
+        <div>Time Since Last Bid: time here</div>
+        <div>Place Your Bid: <button>Bid 1</button> <button>Bid 2</button> <button>Bid 3</button> </div> 
+        <div>OR</div>
+        <div><input placeholder="Type Here"></input> <button>Bid</button></div>
       </div>
     </div>
   );
