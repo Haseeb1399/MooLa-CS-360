e909db798c463f4bdc12fb810c4ad8de1725866e
Irtza <irtzatariq23@gmail.com>
2022-04-17 20:35:56 +0500

    filter working

--- backend/routes/Ad/ad.js
+++ backend/routes/Ad/ad.js
@@ -79,17 +79,31 @@ router.route('/post/watchlist').post((req,res) => {
 
 
 router.route('/marketplace').post((req,res) => {
-    
-    user.Ad.find({ad_type:req.body.ad_type, sold:false})
-    .populate(['animal_id',"seller_id"]) //Make sure sold is false
-    .exec((err,response)=>{
-        if (err == null)
-        {
-            res.json(response);
-        }else{
-            res.json({error:err})
-        }
-    })
+    sex = req.body.sex;
+   if(sex == "Male" || sex == "Female") {
+        user.Ad.find({ad_type:req.body.ad_type, sold:false,sex:sex})
+        .populate(['animal_id',"seller_id"]) //Make sure sold is false
+        .exec((err,response)=>{
+            if (err == null)
+            {
+                res.json(response);
+            }else{
+                res.json({error:err})
+            }
+        })
+    }
+    else {
+        user.Ad.find({ad_type:req.body.ad_type, sold:false})
+        .populate(['animal_id',"seller_id"]) //Make sure sold is false
+        .exec((err,response)=>{
+            if (err == null)
+            {
+                res.json(response);
+            }else{
+                res.json({error:err})
+            }
+        })
+    }
     //console.log(result)
 })
 
--- src/components/Customer/marketplace/marketplace.js
+++ src/components/Customer/marketplace/marketplace.js
@@ -13,6 +13,12 @@ const Marketplace = ()=>{
     let animals = [];
     let sellers = [];
     const [ads, setAds] = useState([]);
+    const [animal, setAnimal] = useState("");
+    const [sex, setSex] = useState("");
+    const [Lweight, setLweight] = useState("");
+    const [Uweight, setUweight] = useState("");
+
+
     let type = 0;
     
 
@@ -68,6 +74,29 @@ const Marketplace = ()=>{
         })
       },[])
 
+      function Filter() {
+        useEffect(()=>{
+        
+            axios.post(process.env.REACT_APP_LOCAL_KEY+'/Ad/marketplace',{ad_type:type,sex:sex}).then(function (res) {
+              // console.log(res.data.message);
+              // feedDisplay.insertAdjacentHTML("beforeend",<div><h3>`${res.data.message}</h3></div>)
+              // console.log(res) //Console me data arha he isse display karna he
+            //   setAds(res.data.message);
+            //   console.log(res.data.message);
+            //   console.log("here");
+            //   console.log(ads);
+                console.log("here");
+                console.log(res);
+                setAds(res.data);
+                
+              
+              //feedDisplay.insertAdjacentHTML("beforeend", res.data.data.message[0])
+            })
+            .catch(function(err) {
+              console.log(err)
+            })
+          },[])
+      }
       return (
           <div class="App">
               <div class="filter-marketplace">
@@ -83,19 +112,19 @@ const Marketplace = ()=>{
 
                     <div class="checkcontainer-marketplace">
                     <label for="cow">
-                        <input class="checkbox-marketplace" type="checkbox" id="cow"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="cow" value = "cow" onChange={(event) => setAnimal(event.target.value)}/>
                         Cow
                     </label>
                     <label for="goat">
-                        <input class="checkbox-marketplace" type="checkbox" id="goat"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="goat" value = "goat" onChange={(event) => setAnimal(event.target.value)}/>
                         Goat
                     </label>
                     <label for="camel">
-                        <input class="checkbox-marketplace" type="checkbox" id="camel"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="camel" value = "camel" onChange={(event) => setAnimal(event.target.value)}/>
                         Camel
                     </label>
                     <label for="buffalo">
-                        <input class="checkbox-marketplace" type="checkbox" id="buffalo"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="buffalo" value = "buffalo" onChange={(event) => setAnimal(event.target.value)}/>
                         Buffalo
                     </label>
                     </div>
@@ -106,11 +135,11 @@ const Marketplace = ()=>{
 
                     <div class="checkcontainer-marketplace">
                     <label for="male">
-                        <input class="checkbox-marketplace" type="checkbox" id="male"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="male" value = "Male" onChange={(event) => setSex(event.target.value)}/>
                         Male
                     </label>
                     <label for="female">
-                        <input class="checkbox-marketplace" type="checkbox" id="female"/>
+                        <input class="checkbox-marketplace" type="checkbox" id="female" value = "Female" onChange={(event) => setSex(event.target.value)}/>
                         Female
                     </label>
                     </div>
@@ -119,13 +148,13 @@ const Marketplace = ()=>{
                 <div class="weight-marketplace"> 
                     <div class="text-marketplace subtitle-marketplace">Weight</div>
 
-                    <input class="textboxes-marketplace" type="text" placeholder="KG" id="lowest-weight" />
+                    <input class="textboxes-marketplace" type="text" placeholder="KG" id="lowest-weight" value = "0" onChange={(event) => setLweight(event.target.value)}/>
                     - 
-                    <input class="textboxes-marketplace" type="text" placeholder="KG" id="highest-weight" />
+                    <input class="textboxes-marketplace" type="text" placeholder="KG" id="highest-weight" value = "0" onChange={(event) => setUweight(event.target.value)}/>
                 </div>
                 
                 </form>
-                <input class="button-marketplace" type="submit" form="myform" value="Filter"/>
+                <input class="button-marketplace" type="submit" form="myform" value="Filter" onClick={Filter}/>
             
             </div>
             <div class="posts-container-marketplace">
@@ -142,41 +171,46 @@ const Marketplace = ()=>{
                         console.log(watch)
                         Submit()
                     }
-                    return(
-                        <div class="posts-marketplace">
-        
-                            <div class="header-marketplace">
-                            <img src={img} class="profilepic-marketplace"/>
-                            <div class="header-lines-marketplace">
-                                <div class="header-text-marketplace">{val.seller_id.username}</div>
-                                <div class="header-text-marketplace subtex-marketplacet"></div>
-                            </div>
-                            </div>
 
-                            <img id="myimg" src={val.photo} class="post-picture-marketplace" />
-                            {console.log(val.photo)}
-                            <div class="post-body-marketplace">
-                            <div class="body-lines-marketplace">
-                                <div class="body-text-marketplace">Sex: {val.animal_id.sex} </div>
-                                <div class="body-text-marketplace">No. of teeth: {val.animal_id.teeth}</div>
-                                <div class="body-text-marketplace">Weight: {val.animal_id.weight}</div>
-                                <div class="body-text-marketplace">Color: {val.animal_id.color} </div>
-                                <div class="body-text-marketplace">Breed: {val.animal_id.type}</div>
-                                <div class="body-text-marketplace">Age:{val.animal_id.age} </div>
-                                <div class="body-text-marketplace">Injuries: {val.animal_id.injury}</div>
-                                <div class="body-text-marketplace">Price: {val.animal_id.price}</div>
-                            </div>
-                            </div>
+                    if(sex == "" || sex == val.animal_id.sex) {
+                        if(animal == "" || animal == val.animal_id.type){
+                            return(
+                                <div class="posts-marketplace">
+                
+                                    <div class="header-marketplace">
+                                    <img src={img} class="profilepic-marketplace"/>
+                                    <div class="header-lines-marketplace">
+                                        <div class="header-text-marketplace">{val.seller_id.username}</div>
+                                        <div class="header-text-marketplace subtex-marketplacet"></div>
+                                    </div>
+                                    </div>
 
-                            <div class="post-buttons-marketplace">
-                            <a class="button-marketplace OpenAd-marketplace">
-                                <Link to={"/view/animalAdd"} state={{data:val}}>Open</Link>
-                            </a>
-                            <a onClick={Add} class="button-marketplace Watchlist-marketplace">Add to Watchlist</a>
-                            </div>
+                                    <img id="myimg" src={val.photo} class="post-picture-marketplace" />
+                                    {console.log(val.photo)}
+                                    <div class="post-body-marketplace">
+                                    <div class="body-lines-marketplace">
+                                        <div class="body-text-marketplace">Sex: {val.animal_id.sex} </div>
+                                        <div class="body-text-marketplace">No. of teeth: {val.animal_id.teeth}</div>
+                                        <div class="body-text-marketplace">Weight: {val.animal_id.weight}</div>
+                                        <div class="body-text-marketplace">Color: {val.animal_id.color} </div>
+                                        <div class="body-text-marketplace">Breed: {val.animal_id.type}</div>
+                                        <div class="body-text-marketplace">Age:{val.animal_id.age} </div>
+                                        <div class="body-text-marketplace">Injuries: {val.animal_id.injury}</div>
+                                        <div class="body-text-marketplace">Price: {val.animal_id.price}</div>
+                                    </div>
+                                    </div>
 
-                    </div>
-                    )
+                                    <div class="post-buttons-marketplace">
+                                    <a class="button-marketplace OpenAd-marketplace">
+                                        <Link to={"/view/animalAdd"} state={{data:val}}>Open</Link>
+                                    </a>
+                                    <a onClick={Add} class="button-marketplace Watchlist-marketplace">Add to Watchlist</a>
+                                    </div>
+
+                            </div>
+                            )
+                    }
+                }
                 })
             }
             </div>
