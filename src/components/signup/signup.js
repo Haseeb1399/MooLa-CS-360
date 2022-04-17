4d90732757ef3aa11c92c3490bd20b3020777e0c
Ebad <ebadrehman2000@gmail.com>
2022-04-18 02:54:00 +0500

    signup and requests

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -15,23 +15,25 @@ const Signup = () => {
         <h1 className="subtitle">I want to sign up as a</h1>
       </div> 
       <div className="signup">
-        <img src={buyer} className="pic left" />
+        <img src={buyer} className="pic-signup left" />
         <Link to ='/signup/customer'><button className="buttons">Customer</button></Link>
       </div>
 
       <div className="signup">
-        <img src={seller} className="pic middle" />
+        <img src={seller} className="pic-signup middle" />
         <Link to ='/signup/seller'><button className="buttons">Seller</button></Link>
       </div>    
       <div className="signup">
-        <img src={butcher} className="pic right" />
+        <img src={butcher} className="pic-signup right" />
         <Link to ='/signup/butcher'><button className="buttons">Butcher</button></Link>
       </div>  
-      {/*<div className="signup">
-        <Link to ='/signup/admin'><button className="button-admin">Become an Admin</button></Link>
-  </div>*/}  
+      
       <Link to = '/'> <button className="backbutton">Go Back</button></Link>    
+      <div>
+      <Link to ='/signup/admin'><button className="button-admin">Become an Admin</button></Link>
+      </div>
     </div>
+
   );
 };
 
